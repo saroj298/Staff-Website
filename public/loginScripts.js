c6d92f6c014a8acf2dd9f94a2930c274d821c3b6
Jack <J.M.Jones3@wlv.ac.uk>
2025-03-04 21:57:42 +0000

    Fixed login error reporting. There were issues causing all invalid submissions to be reported as "an unknown error occurred" or similar; this was fixed by adding an else around the try statement so the errors from the two checks before wouldn't be overwritten. Fixed invalid login information error reporting by removing unnecessary reports and just throwing an error with the message, which is now properly handled in a catch statement.

--- public/loginScripts.js
+++ public/loginScripts.js
@@ -29,45 +29,49 @@ async function validateLogin(event){
     //Ensure no blank fields
     if (!email || !document.getElementById("password").value) {
         errorDisplay.innerText = "One or more fields have been left blank.";
+        return;
     }
 
     //Ensure correct email formating
     else if(!(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).test(email)) {
-        errorDisplay.innerText = "Invalid email format. Please enter a valid email"
+        errorDisplay.innerText = "Invalid email format. Please enter a valid email";
+        return;
     }
 
-    try {
-        //Encrypt password and send to server for verification
-        const encryptedPasswordBase64 = await encryptString(document.getElementById("password").value);
-        const encryptedEmailBase64 = await encryptString(email);
-        const response = await fetch ("/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({encryptedEmailBase64, encryptedPasswordBase64})
-        });
+    else {
+        try {
+            //Encrypt password and send to server for verification
+            const encryptedPasswordBase64 = await encryptString(document.getElementById("password").value);
+            const encryptedEmailBase64 = await encryptString(email);
+            const response = await fetch ("/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({encryptedEmailBase64, encryptedPasswordBase64})
+            });
 
-        //Do action based on response from server
-        if (!response.ok) {
-            throw new Error(response.satus === 401 ? "Invalid email or password." : "An unexpected error occured.");
+            //Do action based on response from server
+            if (!response.ok) {
+                throw new Error(response.status === 401 ? "Invalid email or password." : null);
+            }
+            const data = await response.json();
+            if (data.success) {
+                localStorage.setItem("token", data.token);
+                window.location.href = "admin-panel.html";
+            }
+            else {
+                throw new Error(data.message);
+            }
         }
-        const data = await response.json();
-        if (data.success) {
-            localStorage.setItem("token", data.token);
-            window.location.href = "admin-panel.html";
-        }
-        else {
-            throw new Error(data.message || "An unexpected error occured.");
-        }
-    }
 
-    //Any unexpected errors
-    catch (error){
-        errorDisplay.innerText = "An unexpected error occured. Please try again.";
-        console.error("Error during login: " + error)
-    }
+        //Any errors
+        catch (error){
+            errorDisplay.innerText = error.message || "An unexpected error occured. Please try again.";
+            console.error("Error during login: " + error)
+        }
 
-    //After function complete clear password field of any data
-    finally {
-        document.getElementById("password").value = "";
+        //After function complete clear password field of any data
+        finally {
+            document.getElementById("password").value = "";
+        }
     }
 }
