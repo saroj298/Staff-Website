e0fa9a08a3849b71e8c1df83cad05802fe0b5929
Jack <J.M.Jones3@wlv.ac.uk>
2025-03-08 01:08:21 +0000

    Added background image cycling/swapping. Added background swapping using backgroundCycle.js to cycle between multiple background images on a 30s timer. Changed scaleHandler.js – removed a lot of bloat that wasn't necessary; instead, it's handled by CSS.

---
+++ public/index/javascript/backgroundCycle.js
@@ -0,0 +1,23 @@
+const backgroundImages = [
+    "../../index/images/indexBackground1.jpg",
+    "../../index/images/indexBackground2.jpg",
+    "../../index/images/indexBackground3.jpg"
+];
+let backgroundIndex = 0;
+
+function cycleBackgroundImage(){
+    const backgroundContainer = document.getElementById("backgroundImage");
+    backgroundIndex = (backgroundIndex+1)%backgroundImages.length;
+    const newBackground = document.createElement("div");
+    newBackground.classList.add("backgroundTransition");
+    newBackground.style.backgroundImage = "url(" + backgroundImages[backgroundIndex] + ")";
+    backgroundContainer.appendChild(newBackground);
+    window.getComputedStyle(newBackground).opacity;
+    newBackground.style.opacity = 1;
+    setTimeout(() => {
+        while (backgroundContainer.children.length > 1) {
+            backgroundContainer.removeChild(backgroundContainer.firstChild);
+        }
+    }, 1500);
+} 
+setInterval(cycleBackgroundImage, 30000);
--- public/index/javascript/scaleHandler.js
+++ public/index/javascript/scaleHandler.js
@@ -3,17 +3,6 @@ function scaleContainer(){
     const scaleFactor = mode === "landscape" ? Math.min(window.innerWidth/1920, window.innerHeight/1080): Math.min(window.innerWidth/1080, window.innerHeight/1920);
     if (mode === "landscape") {
         document.documentElement.style.setProperty("--scale", scaleFactor);
-        const backgroundImage = document.getElementById("backgroundImage");
-        backgroundImage.style.width = (1920 * scaleFactor) + "px";
-        backgroundImage.style.height = (1080 * scaleFactor) + "px";
-        if (backgroundImage.offsetWidth < window.innerWidth) {
-            backgroundImage.style.width = (1920*(scaleFactor*(window.innerWidth/backgroundImage.offsetWidth))) +"px";
-            backgroundImage.style.height = (1080*(scaleFactor*(window.innerWidth/backgroundImage.offsetWidth))) +"px";
-        }
-        else if (backgroundImage.offsetHeight <= window.innerHeight) {
-            backgroundImage.style.width = (1920*(scaleFactor*(window.innerHeight/backgroundImage.offsetHeight))) +"px";
-            backgroundImage.style.height = (1080*(scaleFactor*(window.innerHeight/backgroundImage.offsetHeight))) +"px";
-        }
     }
 }
 document.addEventListener("DOMContentLoaded", scaleContainer);
